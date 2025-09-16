fb90f9c81335e8005ca225e9eb8f8052b9c25caa
jseimKSU <jseim@ksu.edu>
2025-09-16 21:41:56 +0000

    added stuff to vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,21 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const users = ref([])
+
+fetch('/users/').then(response => response.json())
+.then((response) => {
+  users.value = response
+}).catch((e) => {
+  console.error(e)
+})
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>List of Users</h1>
+  <ul>
+    <li v-for="user in users" :key="user.id">{{ user.username }}</li>
+  </ul>
 </template>
 
 <style scoped></style>
--- client/vite.config.js
+++ client/vite.config.js
@@ -15,4 +15,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      '/users': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
